d6d4d1a99be6f024f4f55bd02f2b2a15ca6a3917
JYN <hellodbsk01@gmail.com>
2025-05-06 01:39:04 +0900

    plz

--- components/main/subcomponents/TextBox.tsx
+++ components/main/subcomponents/TextBox.tsx
@@ -2,12 +2,11 @@
 import { useState } from "react";
 
 const TextBox = ({ text }: { text: string }) => {
-  const [percent, setPercent] = useState(0);
   return (
     <div className="textBox px-4 py-1 mb-4 flex items-center w-fit">
       <span className="dot w-1 h-1 bg-[#444] inline-block mr-2.5"></span>
       <span className=" font-[500] text-sm">{text}</span>
-      <div className={`gauge w-[${setPercent(percent)}%]`}></div>
+      <div className="gauge"></div>
     </div>
   );
 };
